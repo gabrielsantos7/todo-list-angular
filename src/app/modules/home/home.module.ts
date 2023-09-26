146adddf6806bdd17ee7ded7b9348a0c6141be90
Gabriel Silva Santos <gabrielss432112@gmail.com>
2023-09-26 20:07:54 -0300

    remove header component

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './components/header/header.component';
-import { CheckIconComponent } from './components/check-icon/check-icon.component';
 import { TodoCardComponent } from './components/todo-card/todo-card.component';
 import { TodoComponent } from './components/todo/todo.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    HeaderComponent,
-    CheckIconComponent,
     TodoCardComponent,
     TodoComponent,
   ],
@@ -20,7 +16,6 @@ import { FormsModule } from '@angular/forms';
   ],
 
   exports: [
-    HeaderComponent,
     TodoCardComponent,
     TodoComponent
   ]
